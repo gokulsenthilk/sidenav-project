82f7221de1c158e150e7456323459d75b5ec7b5f
Gokul Senthil <gokulsenthilkuppusamy@gmail.com>
2023-01-26 08:02:42 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,112 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sidenav-project';
+  openSidebar: boolean = true;
+
+  menuSidebar = [
+    {
+      link_name: 'Dashboard',
+      link: '/dashboard',
+      icon: 'home',
+      sub_menu: [],
+    },
+    {
+      link_name: 'Category',
+      link: null,
+      icon: 'view_module',
+      sub_menu: [
+        {
+          link_name: 'HTML & CSS',
+          link: '/html-n-css',
+        },
+        {
+          link_name: 'JavaScript',
+          link: '/javascript',
+        },
+        {
+          link_name: 'PHP & MySQL',
+          link: '/php-n-mysql',
+        },
+      ],
+    },
+    {
+      link_name: 'Posts',
+      link: null,
+      icon: 'vertical_split',
+      sub_menu: [
+        {
+          link_name: 'Web Design',
+          link: '/posts/web-design',
+        },
+        {
+          link_name: 'Login Form',
+          link: '/posts/login-form',
+        },
+        {
+          link_name: 'Card Design',
+          link: '/posts/card-design',
+        },
+      ],
+    },
+    {
+      link_name: 'Analytics',
+      link: '/analytics',
+      icon: 'view_module',
+      sub_menu: [],
+    },
+    {
+      link_name: 'Chart',
+      link: '/chart',
+      icon: 'view_module',
+      sub_menu: [],
+    },
+    {
+      link_name: 'Plugins',
+      link: null,
+      icon: 'view_module',
+      sub_menu: [
+        {
+          link_name: 'UI Face',
+          link: '/ui-face',
+        },
+        {
+          link_name: 'Pigments',
+          link: '/pigments',
+        },
+        {
+          link_name: 'Box Icons',
+          link: '/box-icons',
+        },
+      ],
+    },
+    {
+      link_name: 'Explore',
+      link: '/explore',
+      icon: 'view_module',
+      sub_menu: [],
+    },
+    {
+      link_name: 'History',
+      link: '/history',
+      icon: 'view_module',
+      sub_menu: [],
+    },
+    {
+      link_name: 'Setting',
+      link: '/setting',
+      icon: 'view_module',
+      sub_menu: [],
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  showSubmenu(itemEl: HTMLElement) {
+    itemEl.classList.toggle('showMenu');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnalyticsComponent } from './views/analytics.component';
+import { HTML_n_CSSComponent } from './views/category/html-n-css.component';
+import { JavascriptComponent } from './views/category/javascript.component';
+import { PHP_n_MySQLComponent } from './views/category/php-n-mysql.component';
+import { ChartComponent } from './views/chart.component';
+import { DashboardComponent } from './views/dashboard.component';
+import { DefaultComponent } from './views/default.component';
+import { ExploreComponent } from './views/explore.component';
+import { HistoryComponent } from './views/history.component';
+import { BoxIconsComponent } from './views/plugins/box-icons.component';
+import { PigmentsComponent } from './views/plugins/pigments.component';
+import { UIFaceComponent } from './views/plugins/ui-face.component';
+import { CardDesignComponent } from './views/posts/card-design.component';
+import { LoginFormComponent } from './views/posts/login-form.component';
+import { WebDesignComponent } from './views/posts/web-design.component';
+import { SettingComponent } from './views/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    DashboardComponent,
+    HTML_n_CSSComponent,
+    JavascriptComponent,
+    PHP_n_MySQLComponent,
+    WebDesignComponent,
+    LoginFormComponent,
+    CardDesignComponent,
+    AnalyticsComponent,
+    ChartComponent,
+    UIFaceComponent,
+    PigmentsComponent,
+    BoxIconsComponent,
+    ExploreComponent,
+    HistoryComponent,
+    SettingComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
